e6af8ad062cd21624d4dae9310ce852e37827835
Doxteam <doxteam@Doxteams-MacBook-Pro-2.local>
2023-07-11 16:18:14 +0200

    initial commit

--- API/product/product.controller.js
+++ API/product/product.controller.js
@@ -1,27 +1,7 @@
 const ProductService = require("./product.service");
 
 class ProductController {
-   //1.Get all products
-   static async getAllProducts(req, res) {
-    try {
-      const products = await ProductService.getAllProducts();
-      res.status(200).send(products);
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  }
-
-  //2.Get by ID
-  static async getProductById(req, res) {
-    try {
-      const productId = req.params.id;
-      const product = await ProductService.getProductById(productId);
-      res.status(200).send(product);
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  }
-  //3.Create product
+  //1.Create product
   static async createProduct(req, res) {
     try {
       const productData = req.body;
@@ -33,32 +13,52 @@ class ProductController {
       res.status(400).send(error);
     }
   }
-
-
-  //4.Update Product
-  static async updateProduct(req, res) {
+  //2.Get all products
+  static async getAllProduct(req, res) {
     try {
-      const productId = req.params.id;
-      const productData = req.body;
-      const product = await ProductService.updateProduct(
-        productId,
-        productData
-      );
-      console.log(product);
-      res.status(200).send(product);
+      const products = await ProductService.getAllProducts();
+      res.status(200).send(products);
     } catch (error) {
       res.status(400).send(error);
     }
   }
-  //5.Delete Product
-  static async deleteProduct(req, res) {
+
+  //3.Get by ID
+  static async getProductById(req,res){
     try {
-      const productId = req.params.id;
-      console.log(productId);
-      const product = await ProductService.deleteProduct(productId);
-      res.status(200).send(product);
+        const productId=req.params.id;
+        const product=await ProductService.getProductById(productId);
+        res.status(200).send(product);
+        
     } catch (error) {
-      res.status(400).send(error);
+        res.status(400).send(error);
+    }
+  }
+
+  //4.Delete Product
+  static async deleteProduct(req,res){
+    try {
+        const productId=req.params.id;
+        const product=await ProductService.deleteProduct(productId);
+        res.status(200).send(product)
+        
+    } catch (error) {
+        res.status(400).send(error);
+    }
+  }
+
+  //5/Update Product
+  static async updateProduct(req,res){
+    try {
+
+        const productId=req.params.id;
+        const productData=req.body;
+        const product=await ProductService.updateProduct(productId,productData);
+        console.log(product);
+        res.status(200).send(product);
+    } catch (error) {
+        res.status(400).send(error);
+        
     }
   }
 }
