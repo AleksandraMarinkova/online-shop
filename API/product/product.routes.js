fcfee967aa4610bc35da06433107a1608bb2d76f
Doxteam <doxteam@Doxteams-MacBook-Pro-2.local>
2023-07-12 11:17:08 +0200

    product update and delete

--- API/product/product.routes.js
+++ API/product/product.routes.js
@@ -4,12 +4,14 @@ const ProductController = require("./product.controller");
 // http://localhost:3000/product
 
 // 1. Get all products
-router.get("/", ProductController.getAllPrducts);
+router.get("/", ProductController.getAllProducts);
 // 2. Get product by id
 router.get("/:id",ProductController.getProductById)
 // 3. Create product
 router.post("/", ProductController.createProduct);
 // 4. Update product
+router.patch("/:id",ProductController.updateProduct);
 // 5. Delete product
+router.delete("/:id",ProductController.deleteProduct);
 
 module.exports = router;
